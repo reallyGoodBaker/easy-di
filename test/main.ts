1b23acf67ff30eae7bbbd01507c2c5a58724b0f0
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2022-07-13 00:07:58 +0800

    Update main.ts

--- test/main.ts
+++ test/main.ts
@@ -4,6 +4,7 @@ import {IA} from './interface-A'
 
 class App {
     constructor(
+        //将 IA 对应的接口注入 App 类
         @IA private readonly a: IA,
     ) {}
 
@@ -12,8 +13,14 @@ class App {
     }
 }
 
+//通过初始化服务构建类实例
 new InstantiationService(
+    //为初始化服务提供接口identifier与实现类的绑定
     new ServiceCollection([
+        //[Identifier, {new(): any}, any[]]
+        //Index 0: createIdentifier<T>(uid: string) 函数返回的Identifier对象
+        //Index 1: 实现类
+        //Index 2: 初始化类时的静态参数（选填）
         [IA, A]
     ])
 ).createInstance<App>(App).startApp()
